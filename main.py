82035bea38336697e4e92f22632b0d3cbccc79ef
Negative <negative_amir@yahoo.com>
2016-07-24 18:59:46 +0430

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ sys.setdefaultencoding("utf-8")
 
 bot = telebot.TeleBot('TOKEN')
 admin = 68747297
+botusername = 'Usernamebot'
 
 @bot.message_handler(func=lambda m: m.text)
 def n(m):
     text = m.text
     id = m.from_user.id
-    if re.match('^/(id)$',text):
+    if re.match('^/(id|who)$',text):
         bot.send_message(m.chat.id, m.from_user.id)
-    if re.match('^([/]help)$',text):
+    if re.match('^/(help|start)$',text):
         bot.send_message(m.chat.id, """
 1> /id
 2> <code>send url png|jpg</code>
@@ -34,19 +35,31 @@ def n(m):
             dw(text,'s.jpg')
             bot.send_photo(m.chat.id, open('s.jpg'))
             os.remove('s.jpg') 
+        if re.match('(ftp|http|https)://.*\.(zip)$',text):
+            bot.send_message(m.chat.id, 'ok wait')
+            dw(text,'file.zip')
+            bot.send_photo(m.chat.id, open('file.zip'))
+            os.remove('file.zip')
     if m.chat.type == 'group' or  m.chat.type == 'supergroup':
         if m.reply_to_message:
-            if m.reply_to_message.from_user.username == 'test_isdafsbot':
+            if m.reply_to_message.from_user.username == botusername':
                 if re.match('(ftp|http|https)://.*\.(png)$',text):
                     bot.send_message(m.chat.id, 'ok wait')
                     dw(text,'s.png')
                     bot.send_photo(m.chat.id, open('s.png'))
                     os.remove('s.png') 
-                if re.match('(ftp|http|https)://.*\.(jpg)$',text):
-                    bot.send_message(m.chat.id, 'ok wait')
-                    dw(text,'s.jpg')
-                    bot.send_photo(m.chat.id, open('s.jpg'))
-                    os.remove('s.jpg') 
+                if re.match('(ftp|http|https)://.*\.(jpg)$',text):      #
+                    bot.send_message(m.chat.id, 'ok wait')              # pic download File (Group by reply)
+                    dw(text,'s.jpg')                                    #
+                    bot.send_photo(m.chat.id, open('s.jpg'))          
+                    os.remove('s.jpg')
+                    print 'Remove jpg file'
+                if re.match('(ftp|http|https)://.*\.(zip)$',text):
+                    bot.send_message(m.chat.id, 'ok wait')              #
+                    dw(text,'file.zip')                                 # zip files
+                    bot.send_photo(m.chat.id, open('file.zip'))         #
+                    os.remove('file.zip')
+                    print 'Remove zip file'
 
 
-bot.polling()
+bot.polling(True)
